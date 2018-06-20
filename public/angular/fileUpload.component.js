e14bf59b72ef46ae04edf0198e44baf8f16e0c88
DAWS\horva <horvath.dawson@gmail.com>
2018-06-20 08:26:28 -0700

    fileupload directive

---
+++ public/angular/fileUpload.component.js
@@ -0,0 +1,28 @@
+angular.module('myApp')
+.component('fileUpload', {
+  templateUrl: '../uploadFile.html',
+  controller: fileUpload,
+  controllerAs: 'file'
+});
+
+function fileUpload() {
+  var vm = this;
+  vm.fileUpload = fileUpload;
+
+  function fileUpload() {
+    var file = vm.spreadsheet;
+    var payload = new FormData();
+    payload.append('file', file);
+    $http.post('/file/upload', payload, {
+        transformRequest: angular.identity,
+        headers: {
+          'Content-Type': undefined
+        }
+      })
+      .then(function(data) {
+        XLSX.writeFile(data.data, "vinInformation.xlsx");
+      }, function(err) {
+        console.log("error adding value", err);
+      });
+  }
+}
